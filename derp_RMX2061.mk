919f30ecfd925f5a86f6efeb2107d3b4da87afe9
pri0818 <priyanshusinghal0818@gmail.com>
2021-12-26 15:27:06 +0000

    RMX2061: DerpFest Official BringUp
    Signed-off-by: pri0818 <priyanshusinghal0818@gmail.com>

--- derp_RMX2061.mk
+++ derp_RMX2061.mk
@@ -9,16 +9,16 @@ $(call inherit-product, device/realme/RMX2061/device.mk)
 
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
+WITH_GMS := true
 
-WITH_CORE_GAPPS := true
+# Official
+DERP_BUILDTYPE := Official
 
-EXTENDED_BUILD_TYPE := OFFICIAL
-
-# Inherit some common AospExtended stuff
-$(call inherit-product, vendor/aosp/common.mk)
+# Inherit some common stuff
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
 # Device identifier
-PRODUCT_NAME := aosp_RMX2061
+PRODUCT_NAME := derp_RMX2061
 PRODUCT_DEVICE := RMX2061
 PRODUCT_BRAND := Realme
 PRODUCT_MODEL := Realme 6 Pro
@@ -26,10 +26,10 @@ PRODUCT_MANUFACTURER := Realme
 
 PRODUCT_GMS_CLIENTID_BASE := android-realme
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="atoll-user 11 RKQ1.200903.002/ eng.root.04080114.013112 release-keys"
+#PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="walleye-user 8.1.0 OPM1.171019.011 4448085 release-keys"
 
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys"
+BUILD_FINGERPRINT := "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="RMX2061" \
